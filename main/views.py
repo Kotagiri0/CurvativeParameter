d8e0fe32b3074444b0ebc1ee03b506ce6d2e8c51
Kotagiri <alexey.xmas@gmail.com>
2025-09-13 21:30:53 +0300

    fix register

--- main/views.py
+++ main/views.py
@@ -662,7 +662,7 @@ def register(request):
         form = RegisterForm(request.POST)
         if form.is_valid():
             user = form.save()
-            login(request, user)
+            login(request, user, backend='django.contrib.auth.backends.ModelBackend')
             return redirect('home')
     else:
         form = RegisterForm()
